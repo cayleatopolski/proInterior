4f0d0422a667abe02e7e99082930527cddefc3e3
cayleatopolski <woahitscaylea@gmail.com>
2021-01-06 13:59:09 -0500

    Added constants folder with footer links, nav links, and mock data files.

---
+++ src/constants/footer_links.ts
@@ -0,0 +1,17 @@
+export const nav_links = [
+    { title: 'HomePage',
+      link: '\home'
+    },
+    { title: 'Portfolio',
+      link: '#'
+    },
+    { title: 'Projects',
+      link: '#'
+    },
+    { title: 'Blog',
+      link: '#'
+    },
+    { title: 'Contact',
+      link: '#'
+    },
+]
